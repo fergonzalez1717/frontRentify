4aa31575ad863783a1300a9ad6c7a0b0c1a5e30d
davidolra <da.olaver@duocuc.cl>
2025-09-23 00:55:50 -0300

    se realiza modifciacion de boton enviar

--- rentify/validacionForm.js
+++ rentify/validacionForm.js
@@ -1,7 +1,7 @@
 // validacionForm.js
 (() => {
   'use strict';
-  
+
   const forms = document.querySelectorAll('.needs-validation');
 
   Array.from(forms).forEach(form => {
@@ -9,6 +9,10 @@
       if (!form.checkValidity()) {
         event.preventDefault();
         event.stopPropagation();
+      } else {
+        // ✅ Si pasa la validación, redirige a inicio.html
+        event.preventDefault(); // evita recarga del form
+        window.location.href = "inicio.html";
       }
       form.classList.add('was-validated');
     }, false);
